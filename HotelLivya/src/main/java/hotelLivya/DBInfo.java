ca268aa67cb1dca2f4e55c3dd384f42c5c604713
Rohan <152033186+rohan848382@users.noreply.github.com>
2024-08-13 18:36:06 +0530

    update

---
+++ HotelLivya/src/main/java/hotelLivya/DBInfo.java
@@ -0,0 +1,10 @@
+package hotelLivya;
+
+public interface DBInfo {
+
+	public static final String driver= "oracle.jdbc.driver.OracleDriver";
+	public static final String url = "jdbc:oracle:thin:@localhost:1522:orcl";
+	public static final String username = "system";
+	public static final String password = "Akash1234";
+	
+}
